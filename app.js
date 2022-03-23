0700fc3f5fca8ce34708beccc3bc628a80a01e75
Brien Thomas <thomasbrien94@gmail.com>
2022-03-22 19:23:20 -0700

    adding foundation of navigation bar and submit email input

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// import functions
-
-// grab DOM elements
-
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+const button = document.getElementById('button');
+button.addEventListener('click', ()=>{
+    //1 store the element with id email-inputs as a variable
+const inputs = document.getElementById('email-input');
+    //2 add the class "hide" to the element
+inputs.classList.toggle('hide');
+    //3 store the element with the id thank-you as a variable
+    // thankYou
+const thankYou = document.getElementById('thank-you');
+    //4 remove the class "hide" from thankYou
+thankYou.classList.toggle('hide');
+});
